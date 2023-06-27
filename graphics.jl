2ad474ed8647ef85a23a8c3135d1fadb72480db3
Colby <cshaw44@gatech.edu>
2023-06-26 21:18:27 -0400

    Removed sampling() from this file, now in rds.jl.

--- graphics.jl
+++ graphics.jl
@@ -1,7 +1,5 @@
 include("rds.jl")
 
-using Statistics
-
 """
     testing(x0::AbstractVector, distribution::Distribution, iterations::Int64)
 
@@ -35,45 +33,6 @@ function testing(traj::AbstractVector)
 end
 
 """
-    testing(x0::AbstractVector, distributions::Vector{Distribution{Univariate, Continuous}}, iterations::Int64)
-
-Plots the trajectory of an initial vector of data for each distribution provided in 'distributions'.
-
-## Arguments
-- `x0`: An initial vector of data points.
-- `distributions`: A vector of possible distributions for a sample space Ω.
-- `func`: f_ω
-- `iterations`: Length of trajectory.
-"""
-function testing(x0::AbstractVector, distributions::Vector{Distribution{Univariate, Continuous}}, func::Function, iterations::Int64)
-    for dist in distributions
-        rds = RDS(Interval{Closed, Closed}(0, 1), 1, dist)
-        data = sampleTraj(rds, iterations, x0, func)
-        dataTraj = Vector{Vector{Float64}}(undef, length(x0))
-
-        plot(title=dist, xlabel="Iterations", ylabel="Values") # Format plot object.
-        
-
-        # Instantiates a vector of vectors to store trajectory of datapoints.
-        for i in eachindex(dataTraj)
-            dataTraj[i] = Float64[]
-        end
-        
-        # Fills vector of trajectories of datapoints.
-        for dataPoint in data
-            for (i, point) in enumerate(dataPoint)
-                push!(dataTraj[i], point)
-            end
-        end
-
-        for i in eachindex(dataTraj)
-            plot!(dataTraj[i], legend=false)
-        end
-        display(current())  # displays the plot associated with the distribution.
-    end
-end
-
-"""
     tracking(traj::AbstractVector)
 
 Tracks the evolution of the distributions of our states over time using a specified function, func (or fω).
@@ -89,30 +48,7 @@ The distribution evolution over time is recorded and displayed by generating a h
 function tracking(traj::AbstractVector)
     # Record and display distribution evolution over time.
     @gif for i in eachindex(traj) 
-        histogram(traj[i], xlims=(0,1), ylims=(0,10000), bins=50, xlabel="Values", ylabel="Frequency") # Plot the values sampled above
+        histogram(traj[i], xlims=(0,1), bins=100, xlabel="Values", ylabel="Frequency", legend=false) 
     end fps = 8
 end
-
-"""
-    sampling(n::Int, distribution::Distribution)
-
-Generate n valid samples from a specified distribution on the interval [0,1].
-
-## Arguments
-- `n::Int`: The number of samples to generate.
-- `distribution::Distribution`: The distribution from which to generate the samples.
-= `precision="false"`: Key parameter determining precision of sample.
-"""
-function sampling(n::Int, distribution::Distribution; precision="false") # Slow when distribution=Normal()
-    # If precision is want, we use BigFloat.
-    if precision=="true"
-         # To randomly sample BigFloats from distribution, we use the inverse CDF function.
-        uni=rand(BigFloat, n)                 
-        samples=quantile(distribution, uni)
-    else
-        samples = rand(distribution, n)
-    end
-    transformed_samples = (samples .- minimum(samples)) / (maximum(samples) - minimum(samples))  # Transform samples to the interval [0, 1]
-    valid_samples = filter(x -> 0 ≤ x ≤ 1, transformed_samples)  # Filter out values outside [lower, upper]
-    return valid_samples
-end
+   
